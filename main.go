184b3984096d9a70479c984d001cba21f70a99bd
roypeco <roy.peco@gmail.com>
2024-03-23 23:10:37 +0900

    add .gitignore

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "net/http"
-
   "github.com/labstack/echo"
 )
 
@@ -11,5 +10,5 @@ func main() {
   e.GET("/", func(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, World!")
   })
-  e.Logger.Fatal(e.Start(":1323"))
+  e.Logger.Fatal(e.Start(":3000"))
 }
