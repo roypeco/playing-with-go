b6ef30db40287c2d0988bf06810cd963c44f08da
roypeco <roy.peco@gmail.com>
2024-03-29 21:12:40 +0900

    We can play rock, paper, scissors.

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+  "fmt"
+  "math/rand"
+  "time"
+  "strconv"
   "net/http"
-  "github.com/labstack/echo"
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
 )
+
+func playRsp(c echo.Context) error {
+  fmt.Println("playRsp is called")
+  var msg string
+  r := rand.New(rand.NewSource(time.Now().UnixNano()))
+  playerHandS := c.Param("hand")
+  playerHandI, _ := strconv.Atoi(playerHandS)
+  cpuHand := r.Intn(3) + 1
+  fmt.Printf("player hand: %d, cpu hand: %d\n", playerHandI, cpuHand)
+  switch (playerHandI - cpuHand + 3) % 3 {
+  case 0:
+    msg = "draw"
+  case 1:
+    msg = "player lose"
+  default:
+    msg = "player win"
+  }
+  return c.String(http.StatusOK, msg)
+}
 
 func main() {
   e := echo.New()
+  e.Use(middleware.Logger())
   e.GET("/", func(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, World!")
   })
+
+  e.GET("/rsp/:hand", playRsp)
+
   e.Logger.Fatal(e.Start(":3000"))
 }
