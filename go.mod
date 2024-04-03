d033d0406c7d9104911feed785605a44e9c059a0
roypeco <roy.peco@gmail.com>
2024-04-03 20:56:29 +0900

    split rsp packages

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module sample-app
+module gosample
 
 go 1.22.1
 
--- main.go
+++ main.go
@@ -1,43 +1,23 @@
+// main.go
+
 package main
 
 import (
-  "fmt"
-  "math/rand"
-  "time"
-  "strconv"
-  "net/http"
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
+    "net/http"
+
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
+    "gosample/packages" // main.goと同じディレクトリにrspパッケージがある場合
 )
 
-func playRsp(c echo.Context) error {
-  fmt.Println("playRsp is called")
-  var msg string
-  r := rand.New(rand.NewSource(time.Now().UnixNano()))
-  playerHandS := c.Param("hand")
-  playerHandI, _ := strconv.Atoi(playerHandS)
-  cpuHand := r.Intn(3) + 1
-  fmt.Printf("player hand: %d, cpu hand: %d\n", playerHandI, cpuHand)
-  switch (playerHandI - cpuHand + 3) % 3 {
-  case 0:
-    msg = "draw"
-  case 1:
-    msg = "player lose"
-  default:
-    msg = "player win"
-  }
-  return c.String(http.StatusOK, msg)
-}
-
 func main() {
-  e := echo.New()
-  e.Use(middleware.Logger())
-  e.GET("/", func(c echo.Context) error {
-    return c.String(http.StatusOK, "Hello, World!")
-  })
+    e := echo.New()
+    e.Use(middleware.Logger())
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World!")
+    })
 
-  e.GET("/rsp/:hand", playRsp)
+    e.GET("/rsp/:hand", packages.PlayRsp)
 
-  e.Logger.Fatal(e.Start(":3000"))
+    e.Logger.Fatal(e.Start(":3000"))
 }
-// comment test
